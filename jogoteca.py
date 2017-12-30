80b06ee43566f8ec9e9e9a829f5672397b179710
wbarbosa0 <wjb00000000-github@yahoo.com.br>
2017-12-30 21:59:47 -0200

    Aula 1 Atividade 9 - Terminado

--- jogoteca.py
+++ jogoteca.py
@@ -2,8 +2,17 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')#'<h1>Olá Flask!</h1>'
+    lista=[Jogo('Super Mario', 'Plataforma', 'NES'),
+           Jogo('Sonic', 'Plataforma', 'SMS'),
+           Jogo('Halo', 'Shooting', 'XBOX')]
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista)#'<h1>Olá Flask!</h1>'
 
 app.run()
