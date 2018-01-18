97788f1456e0bd7365dce7ba1ecfe1562ce5f8aa
wbarbosa0 <wjb00000000-github@yahoo.com.br>
2018-01-17 22:10:24 -0200

    Flask 2 - Aula 1

--- jogoteca.py
+++ jogoteca.py
@@ -62,4 +62,27 @@ def logout():
     flash('Nenhum usuário logado!')
     return redirect(url_for('index'))
 
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('editar')))
+    jogo = jogo_dao.busca_por_id(id)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console, id=request.form['id'])
+    jogo_dao.salvar(jogo)
+    return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    jogo_dao.deletar(id)
+    flash('O jogo foi removido com sucesso!')
+    return redirect(url_for('index'))
+
 app.run(debug=True)
+
