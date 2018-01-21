438de090f11f3e3cd0d7ad40a29b2ed5f210bfcf
wbarbosa0 <wjb00000000-github@yahoo.com.br>
2018-01-21 20:13:43 -0200

    Flask 2 - Aula 5 - Final de curso Erro não solucionado: Nâo se pode entrar em Editar antes de entrar em Novo Jogo, na versão fornecida como gabarito aparentemente também tem esse problema Aberta questão na Alura a respeito disto.

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+SECRET_KEY = 'TrustNo1'
+
+MYSQL_HOST = "127.0.0.1"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = ""
+MYSQL_DB = "jogoteca"
+MYSQL_PORT = 3306
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
